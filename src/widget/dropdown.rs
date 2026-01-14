5ea5aed16cdf385c1ed03b4ffd33d898b7eb983a
manshanko <manshanko@wartide.net>
2026-01-13 20:02:06 -0800

    fix dropdown hover highlight not checking aabb

--- src/widget/dropdown.rs
+++ src/widget/dropdown.rs
@@ -126,6 +126,12 @@ impl super::Widget for DropdownWidget {
             && x >= 0 && x < self.width as i32;
 
         match event.kind {
+            EventKind::MouseMove if !is_inside => {
+                if self.hovered_option.is_some() {
+                    self.hovered_option = None;
+                    control.redraw();
+                }
+            }
             EventKind::MouseMove => {
                 let offset = y - Self::BORDER_SIZE as i32;
                 let opt = offset / Self::ENTRY_HEIGHT as i32;
